fbd64582475a21bbc68b7ab6912eb8a1c0a82b4a
Caro04 <knyta.04@hotmail.com>
2018-10-16 01:14:11 -0600

    apiario

--- resources/views/Apiario/modal.blade.php
+++ resources/views/Apiario/modal.blade.php
@@ -8,10 +8,10 @@ role="dialog" tabindex="-1" id="modal-delete-{{$apiario->id}}">
 				aria-label="Close">
                      <span aria-hidden="true">×</span>
                 </button>
-                <h4 class="modal-title">Eliminar  Colmena</h4>
+                <h4 class="modal-title">Eliminar  Apiario</h4>
 			</div>
 			<div class="modal-body">
-				<p>Confirme si desea Eliminar  Colmena</p>
+				<p>Confirme si desea Eliminar  Apiario</p>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
