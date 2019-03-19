e25719b328f0ef5457f36a9d795f11cf5a26dc51
Carolina Torres Carrillo <43045704+CarolinaT04@users.noreply.github.com>
2019-03-19 00:00:50 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -43,9 +43,7 @@ Route::POST('addApiario','ApiarioController@addApiario');
 Route::POST('editApiario','ApiarioController@editApiario');
 Route::POST('deleteApiario','ApiarioController@deleteApiario');
 
-Route::POST('addUbicacion','UbicacionController@addUbicacion');
-Route::POST('editUbicacion','UbicacionController@editUbicacion');
-Route::POST('deleteUbicacion','UbicacionController@deleteUbicacion');
+
 
 
 });
